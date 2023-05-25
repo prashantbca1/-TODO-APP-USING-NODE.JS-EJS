fe99d7b37401b3ef5e202f50d4faa5a9142c842a
prashant singh <133889505+prashantbca1@users.noreply.github.com>
2023-05-25 17:49:42 +0530

    Create mongoose.js

---
+++ mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+/*const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    mobile: Number
+});
+const userModel = mongoose.model('userModel', UserSchema);
+module.exports = userModel;*/
